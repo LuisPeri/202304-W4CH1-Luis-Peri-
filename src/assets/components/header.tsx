641a1b8bc10eb1621679e59bd3b67adae0eec7f2
Luis <luisperi18@hotmail.com>
2023-05-22 18:48:42 +0200

    Fix header

--- src/assets/components/header.tsx
+++ src/assets/components/header.tsx
@@ -1,15 +1,10 @@
-export function Header ()
-const title = "The pointing gentlemen" 
+export function Header() {
+  const title = "The pointing gentlemen";
   return (
-  <>
-  <header class="main-header">
-      <h1 class="main-title">{title}</h1>
-    </header>
-    <section class="controls">
-      <p class="info">0 gentlemen pointing at you</p>
-      <button class="button button--select">Select all</button>
-    </section>
-  </>
+    <>
+      <header className="main-header">
+        <h1 className="main-title">{title}</h1>
+      </header>
+    </>
   );
-);
-  
+}
