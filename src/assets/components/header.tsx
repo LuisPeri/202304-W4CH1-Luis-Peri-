bea2736187b3750c492cdaaae17cb3fd10aab98d
Luis <luisperi18@hotmail.com>
2023-05-22 18:37:51 +0200

    Add header

--- src/assets/components/header.tsx
+++ src/assets/components/header.tsx
@@ -1,17 +1,15 @@
-type PropsType = {
-  label: string;
-  user: string;
-};
-
-export function Header({ label, user }: PropsType) {
-  const title = "hola mundo";
+export function Header ()
+const title = "The pointing gentlemen" 
   return (
-    <>
-      <header>
-        <h1>{title}</h1>
-        <p>{user}</p>
-        <p>{label}</p>
-      </header>
-    </>
+  <>
+  <header class="main-header">
+      <h1 class="main-title">{title}</h1>
+    </header>
+    <section class="controls">
+      <p class="info">0 gentlemen pointing at you</p>
+      <button class="button button--select">Select all</button>
+    </section>
+  </>
   );
-}
+);
+  
