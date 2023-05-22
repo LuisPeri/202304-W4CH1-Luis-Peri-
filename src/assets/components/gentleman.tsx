0966e86fc59246ac47081f58b63d45b14e33625d
Luis <luisperi18@hotmail.com>
2023-05-22 20:08:29 +0200

    Add gentleman with mock

--- src/assets/components/gentleman.tsx
+++ src/assets/components/gentleman.tsx
@@ -1,32 +1,35 @@
-[
-  {
-    id: 1,
-    name: "Bertin Osborne",
-    status: "Alive",
-    profession: "Youtuber",
-    twitter: "@osbourne",
-    picture: "bertin.jpg",
-    alternativeText: "Osbourne pointing at you",
-    selected: true,
-  },
-  {
-    name: "The Farytale",
-    status: "RIP",
-    profession: "Influencer",
-    twitter: "pending",
-    picture: "fary.jpg",
-    alternativeText: "The Fary pointing at you",
-    id: 2,
-    selected: false,
-  },
-  {
-    id: 3,
-    name: "Julius Churchs",
-    status: "Alive",
-    profession: "Java developer",
-    twitter: "@julius_churchs",
-    picture: "julio.jpg",
-    alternativeText: "Churchs pointing at you",
-    selected: true,
-  },
-];
+import { Mock } from "../../mocks/Mock";
+
+export function Info() {
+  return (
+    <>
+      <li className="gentleman">
+        <div className="gentleman__avatar-container">
+          <img
+            className="gentleman__avatar"
+            src="img/bertin.jpg"
+            alt="Bertin Osbourne pointing at you"
+          />
+          <span className="gentleman__initial">B</span>
+        </div>
+        <div className="gentleman__data-container">
+          <h2 className="gentleman__name">Bertin Osbourne</h2>
+          <ul className="gentleman__data-list">
+            <li className="gentleman__data">
+              <span className="gentleman__data-label">Profession:</span>
+              Java developer
+            </li>
+            <li className="gentleman__data">
+              <span className="gentleman__data-label">Status:</span> Alive
+            </li>
+            <li className="gentleman__data">
+              <span className="gentleman__data-label">Twitter:</span> @osbourne
+            </li>
+          </ul>
+        </div>
+        <i className="icon gentleman__icon fas fa-check"></i>
+        <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
+      </li>
+    </>
+  );
+}
